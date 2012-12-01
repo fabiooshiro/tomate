2b39632b7aac2ed16eb915d31dc4068cbade2ba9
fabio.oshiro <fabio.oshiro@gmail.com>
2012-12-01 01:46:51 -0200

    give a try with jasmine

---
+++ grails-app/conf/TomateResources.groovy
@@ -0,0 +1,9 @@
+modules = {
+
+	tomate{
+		resource url: 'js/jasmine.js', disposition: 'head'
+		resource url: 'js/jasmine-html.js', disposition: 'head'
+		resource url: 'css/jasmine.css', disposition: 'head'
+	}
+
+}
