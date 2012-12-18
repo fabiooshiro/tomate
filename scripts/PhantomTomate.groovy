ea4442af538b626f81613253ba34cd58fd1328e9
fabio.oshiro <fabio.oshiro@gmail.com>
2012-12-18 14:19:13 -0200

    Tentando rodar no bamboo

--- scripts/PhantomTomate.groovy
+++ scripts/PhantomTomate.groovy
@@ -7,8 +7,13 @@ target('default': "Runs project and tomate") {
 	runApp()
     println "Starting phantom"
     def sout = new StringBuffer(), serr = new StringBuffer()
-	def proc = 'phantomjs phantomscript.js'.execute()
-	//def proc = 'ls'.execute()
+	def proc
+	try{
+		proc = 'phantomjs phantomscript.js'.execute()
+	}catch(Throwable e){
+		//TODO: colocar isso no Config
+		proc = '/bamboo/home/opt/phantomjs-1.7.0-linux-x86_64/bin/phantomjs phantomscript.js'.execute()
+	}
 	proc.consumeProcessOutput(sout, serr)
 	proc.waitFor()
 	println "out> $sout err> $serr"
